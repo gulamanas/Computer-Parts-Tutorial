797bdb11a294827f0281fe1a7fac41782cf96aeb
Gulam Anas <g.anas786@gmail.com>
2022-02-13 20:20:35 +0530

    cards

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-const burger = document.querySelector('.burger');
-const navSlider = document.querySelector('.nav_list');
+// const burger = document.querySelector('.burger');
+// const navSlider = document.querySelector('.nav_list');
 
-burger.addEventListener('click', onNavSlide);
+// burger.addEventListener('click', onNavSlide);
 
-function onNavSlide() {
-    burger.classList.toggle('switch');
-    navSlider.classList.toggle('navSwitch');
-}
+// function onNavSlide() {
+//     burger.classList.toggle('switch');
+//     navSlider.classList.toggle('navSwitch');
+// }
